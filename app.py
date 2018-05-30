ba66ea037accaf1ff488f52219085916283a00c8
leonardobenedeti <leonardobenedeti@gmail.com>
2018-05-30 16:46:30 -0300

    Inclusao de lista de cards retornando pela api.

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+class Cards:
+	def __init__(self, title, desc )
+		self.title = title
+		self.desc = desc
+
+listCards = { Cards('Amazing Card #1', 'Card to show how works jsonifi by flask #1'), Cards('Amazing Card #2', 'Card to show how works jsonifi by flask #2')} 
+
 @app.route('/')
 def index():
-	return "API básica em Flask deploiada no heroku!"
+	return jsonify(listCards)
 
 if __name__ == "__main__":
 	app.run()
